6c2c523e46ca0546597e64876a14e9250b339d43
Gabriel Lobo <ga.p.lobo@gmail.com>
2024-02-05 21:01:04 -0300

    - Implemented Paging3

--- app/src/main/java/br/com/movieapp/core/domain/model/Movie.kt
+++ app/src/main/java/br/com/movieapp/core/domain/model/Movie.kt
@@ -2,7 +2,7 @@ package br.com.movieapp.core.domain.model
 
 data class Movie(
     val id: Int,
-    val title: Int,
+    val title: String,
     val voteAverage: Double = 0.0,
     val imageUrl: String = ""
 )
