e221cc73eb2e3956ac1ad688eb0526645fff986b
Gabriel Lobo <ga.p.lobo@gmail.com>
2024-04-04 21:01:09 -0300

    Create Di

---
+++ app/src/main/java/br/com/movieapp/movie_detail_feature/di/MovieDetailsModule.kt
@@ -0,0 +1,36 @@
+package br.com.movieapp.movie_detail_feature.di
+
+import br.com.movieapp.core.data.remote.MovieService
+import br.com.movieapp.movie_detail_feature.data.repository.MovieDetailsRepositoryImpl
+import br.com.movieapp.movie_detail_feature.data.source.MovieDetailsRemoteDataSourceImpl
+import br.com.movieapp.movie_detail_feature.domain.repository.MovieDetailsRepository
+import br.com.movieapp.movie_detail_feature.domain.source.MovieDetailsRemoteDataSource
+import br.com.movieapp.movie_detail_feature.domain.usecase.GetMovieDetailsUseCase
+import br.com.movieapp.movie_detail_feature.domain.usecase.GetMovieDetailsUseCaseImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MovieDetailsModule {
+    @Provides
+    @Singleton
+    fun provideMovieDetailsDataResource(service: MovieService): MovieDetailsRemoteDataSource {
+        return MovieDetailsRemoteDataSourceImpl(service = service)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieDetailRepository(remoteDataSource: MovieDetailsRemoteDataSource): MovieDetailsRepository {
+        return MovieDetailsRepositoryImpl(remoteDataSource = remoteDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetMoviesDetailsUseCase(movieDetailsRepository: MovieDetailsRepository): GetMovieDetailsUseCase {
+        return GetMovieDetailsUseCaseImpl(repository = movieDetailsRepository)
+    }
+}
