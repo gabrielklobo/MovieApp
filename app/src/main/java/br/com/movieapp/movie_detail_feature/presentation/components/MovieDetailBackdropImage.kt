b53e78a5c13c9fe91e314a16e19c947d9709cc10
Gabriel Lobo <ga.p.lobo@gmail.com>
2025-02-24 20:19:19 -0300

    - Created component AsyncImageUrl

--- app/src/main/java/br/com/movieapp/movie_detail_feature/presentation/components/MovieDetailBackdropImage.kt
+++ app/src/main/java/br/com/movieapp/movie_detail_feature/presentation/components/MovieDetailBackdropImage.kt
@@ -6,12 +6,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.movieapp.R
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import br.com.movieapp.core.presentation.components.commom.AsyncImageUrl
 
 @Composable
 fun MovieDetailBackdropImage(
@@ -19,14 +16,8 @@ fun MovieDetailBackdropImage(
     modifier: Modifier
 ) {
     Box(modifier = modifier) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(backdropImageUrl)
-                .crossfade(true)
-                .error(R.drawable.ic_error_image)
-                .placeholder(R.drawable.ic_placeholder)
-                .build(),
-            contentDescription = "",
+        AsyncImageUrl(
+            imageUrl = backdropImageUrl,
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth()
         )
