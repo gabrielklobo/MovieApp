e3b386b8ac452e252f54e43d871ddfc51bca44ba
Gabriel Lobo <ga.p.lobo@gmail.com>
2025-03-26 21:52:38 -0300

    - Created test for getmovie by id

--- app/src/androidTest/java/br/com/movieapp/core/data/local/dao/MovieDao.kt
+++ app/src/androidTest/java/br/com/movieapp/core/data/local/dao/MovieDao.kt
@@ -43,7 +43,7 @@ class MovieDaoTest {
     }
 
     @Test
-    fun getMovies() = runTest {
+    fun test_getMovies_should_return_list_of_movies() = runTest {
         //Given - Nothing
 
         //When
@@ -54,7 +54,7 @@ class MovieDaoTest {
     }
 
     @Test
-    fun getMovies_orderedById() = runTest {
+    fun test_getMovies_should_return_movies_ordered_by_id() = runTest {
         //Given
         val moviesEntity = listOf(
             MovieEntity(movieId = 1, title = "Homem de Ferro 1", imageUrl = "Url1"),
@@ -74,5 +74,21 @@ class MovieDaoTest {
         assertThat(movies[1].movieId).isEqualTo(2)
         assertThat(movies[2].movieId).isEqualTo(3)
         assertThat(movies[3].movieId).isEqualTo(4)
+    }
+
+    @Test
+    fun test_getMovie_should_return_correct_movie_by_id() = runTest {
+        //Given
+        val movieEntity = MovieEntity(movieId = 1, title = "Homem de Ferro 1", imageUrl = "Url1")
+        movieDao.insertMovie(movieEntity)
+
+        val movies = movieDao.getMovies().first()
+        val movieClick = movies[0]
+
+        //When
+        val movieId = movieDao.getMovie(movieClick.movieId)
+
+        //Then
+        assertThat(movieId?.title).isEqualTo(movieClick.title)
     }
 }
